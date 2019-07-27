d87d75b018aa7b90c701b6e682c2970785bc7ec9
Nicos <hfnovember@hotmail.com>
2019-07-27 16:56:59 +0300

    Created doc pages

--- docs/js/init.js
+++ docs/js/init.js
@@ -2,6 +2,8 @@
   $(function(){
 
     $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown();
+    $('.tabs').tabs();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
