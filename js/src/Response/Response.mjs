5399c4080ba739ffe733282f11605404e72bc7ea
Nicos Kasenides <hfnovemeber@hotmail.com>
2019-08-09 10:13:41 +0300

    Added headers in source, new download

--- js/src/Response/Response.mjs
+++ js/src/Response/Response.mjs
@@ -1,4 +1,16 @@
 /**
+ * Created by PaNickApps - 2019
+ * Visit http://www.panickapps.com
+ *
+ * Response - A simple HTTP response modeling library.
+ * Provides a simple model for creating HTTP responses.
+ * Repository: https://github.com/panickapps/Response
+ * Guide: https://panickapps.github.io/Response/
+ *
+ * Apache 2.0 License
+ */
+
+/**
  * Defines a status for responses.
  * @type {string}
  */
